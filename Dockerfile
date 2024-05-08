11af66a206dd528602f357543dcc19c5e00e8907
umontano <54858746+umontano@users.noreply.github.com>
2024-05-08 13:33:38 -0600

    rsid docker and sh file added

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,9 @@
 #custom-args: --platform=linux/arm64,linux/amd64 # multiple target architectures
-FROM rocker/verse
+FROM debian:unstable-slim
 
-ENV common_packs='psych ggcorrplot widyr ggbeeswarm gt gtsummary'
-RUN for pack in "$common_packs"; do Rscript -e "if(!require('${pack}')) install.packages('${pack}')"; done
-## INSTALLERS
-RUN Rscript -e "if(!require('forcats')) install.packages('forcats')"
-RUN Rscript -e "if(!require('GGally')) install.packages('GGally')"
-RUN Rscript -e "if(!require('tidytext')) install.packages('tidytext')"
-RUN Rscript -e "if(!require('flextable')) install.packages('flextable')"
-RUN Rscript -e "if(!require('rmarkdown')) install.packages('rmarkdown')"
-RUN Rscript -e "if(!require('ggplot2')) install.packages('ggplot2')"
-RUN Rscript -e "if(!require('remotes')) install.packages('remotes')"
-RUN Rscript -e "remotes::install_github('r-lib/hugodown')"
-RUN Rscript -e "if(!require('GGally')) install.packages('GGally')"
-RUN Rscript -e "if(!require('cowplot')) install.packages('cowplot')"
-RUN Rscript -e "if(!require('gridExtra')) install.packages('gridExtra')"
-RUN Rscript -e "if(!require('psych')) install.packages('psych')"
-RUN Rscript -e "if(!require('ggcorrplot')) install.packages('ggcorrplot')"
-RUN Rscript -e "if(!require('widyr')) install.packages('widyr')"
-RUN Rscript -e "if(!require('ggbeeswarm')) install.packages('ggbeeswarm')"
-RUN Rscript -e "if(!require('gtsummary')) install.packages('gtsummary')"
-RUN Rscript -e "if(!require('gt')) install.packages('gt')"
-
-
-## INSTALL HUGO 0.80.0
-RUN wget https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_0.80.0_Linux-64bit.tar.gz && \
-tar -zxvf hugo_0.80.0_Linux-64bit.tar.gz && \
-mv hugo /usr/local/bin/ && \
-rm -r hugo*
+COPY install_packs.sh ./
+RUN chmod +x *.sh && sh install_packs.sh
+    #apt-get -y --no-install-recommends install software-properties-common
 
 WORKDIR /app
 CMD ["R"]
---
+++ install_packs.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+program_name=$1
+if command -v "$program_name" >/dev/null 2>&1; then
+    echo "$program_name is installed."
+else
+    echo "$program_name is not installed."
+fi
+exit 0
+
+#!/bin/sh
+arch_dependencies=\
+'icu
+gcc-fortran'
+
+archlinux_r_packs=\
+'kableExtra
+flextable
+reactable'
+
+cran_packs=\
+'shiny
+rmarkdown
+tidyverse
+tidytext
+forcats
+remotes
+dplyr
+ggplot2
+flextable
+psych
+cowplot
+gridExtra'
+
+common_packs=\
+'ggcorrplot
+widyr
+ggbeeswarm
+gt
+gtsummary'
+
+precompiled_repos=\
+"repos = c('https://apache.r-universe.dev', 'https://cloud.r-project.org', 'https://ddsjoberg.r-universe.dev')"
+
+install_with_apt()
+{
+apt-get update
+apt-get clean
+	apt-get -y --no-install-recommends install r-base
+#apt-get install -y r-base-dev || echo __APTTED__"$pack"
+#apt-get install -y r-recommended || echo __APTTED__"$pack"
+apt-get clean || echo __APTTED__"$pack"
+#apt-get autoclean || echo __APTTED__"$pack"
+#apt-get autoremove -y || echo __APTTED__"$pack"
+#dpkg --configure -a || echo __APTTED__"$pack"
+#apt --fix-broken install -y || echo __APTTED__"$pack"
+
+
+echo "$cran_packs" | while read -r pack
+do
+	apt-get -y --no-install-recommends install r-cran-"$pack" || echo __APTTED__"$pack"
+	apt-get clean || echo __APTTED__"$pack"
+	#apt-get autoclean || echo __APTTED__"$pack"
+	#apt-get autoremove -y || echo __APTTED__"$pack"
+	#dpkg --configure -a || echo __APTTED__"$pack"
+	#apt --fix-broken install -y || echo __APTTED__"$pack"
+done
+}
+
+require_conditional_install()
+{
+echo "$common_packs" | while read -r pack
+do 
+	echo ___rscipt_ins_"$pack"
+	Rscript -e "if(!require('${pack}')) install.packages('${pack}', ${precompiled_repos})"
+done
+
+Rscript -e "if(!require(hugodown)) remotes::install_github('r-lib/hugodown', update = 'never')"
+}
+
+
+## CHECK DEBUG FLAG
+if test "$1" = "debug"
+then
+    echo "The positional argument is 'debug'"
+	echo ___DEBUG_FLAG___
+	require_conditional_install
+else
+    echo "The positional argument is not 'debug'"
+	echo ___NO_FLAG___
+	install_with_apt
+	require_conditional_install
+fi
+
+apt-get clean
+
+## arm
+#https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_0.80.0_Linux-ARM.tar.gz
+## arch64v8
+#https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_0.80.0_Linux-ARM64.tar.gz
+## x86_64
+#https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_0.80.0_Linux-64bit.tar.gz
+## mac
+#https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_extended_0.80.0_macOS-64bit.tar.gz
+## windows
+#https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_extended_0.80.0_Windows-64bit.zip
