a113dcec5b9567b2bbf2b41de90040954babcb58
umontano <54858746+umontano@users.noreply.github.com>
2024-06-19 17:51:45 -0600

    texlive added

--- install_packs.sh
+++ install_packs.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-debian_common=\
-'pandoc'
+debian_common='
+pandoc
+texlive-base
+texlive-latex-base
+texlive-fonts-recommended
+texlive-latex-extra
+'
 
 arch_dependencies=\
 'icu
@@ -44,6 +49,24 @@ install_with_apt()
 	apt-get update
 	apt-get clean
 	apt-get -y --no-install-recommends install r-base
+
+
+
+## REMOVE INITIAL AND FINAL NEWLINE CHARACTERS AND REPLACE NEWLINES WITH SPACES
+packages=$(echo "$debian_common" | sed '1s/^[ \t]*//; $s/[ \t]*$//; s/\n/ /g')
+## LOOP THROUGH THE LIST
+echo "Packages to be installed:"
+for package in $packages
+do
+    echo ___TO_BE_I__$package
+	apt-get -y install "$pack" || echo __DEBIAN_INSTALLED__"$pack"
+    echo ___INSTALLED___$package
+	apt-get clean
+done
+
+
+
+
 #apt-get install -y r-base-dev || echo __APTTED__"$pack"
 #apt-get install -y r-recommended || echo __APTTED__"$pack"
 apt-get clean || echo __APTTED__"$pack"
@@ -54,8 +77,8 @@ apt-get clean || echo __APTTED__"$pack"
 echo "$debian_common" | while read -r pack
 do 
 	echo __DEBIAN_TO_I__"$pack"
-	apt-get -y install "$pack" || echo __DEBIAN_INSTALLED__"$pack"
-	apt-get clean
+	#apt-get -y install "$pack" || echo __DEBIAN_INSTALLED__"$pack"
+	#apt-get clean
 done
 
 
