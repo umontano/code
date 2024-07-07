2b1995a3df8a73fa0c677c6105740d18aa4e1308
umontano <54858746+umontano@users.noreply.github.com>
2024-07-07 16:05:57 -0600

    texlive pdflatex tinytex added

--- install_packs.sh
+++ install_packs.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 debian_common='
+wget
+texlive-latex-base
 pandoc
+less
+'
+texlive_debian='
+texlive-latex-base
+texlive-core
 texlive-base
 texlive-latex-base
 texlive-fonts-recommended
@@ -17,7 +24,13 @@ flextable
 reactable'
 
 cran_packs=\
-'lavaan
+'kableextra
+ggally
+semplot
+evaluate
+gert
+v8
+lavaan
 semtools
 erm
 tinytex
@@ -35,8 +48,8 @@ cowplot
 gridExtra'
 
 common_packs=\
-'mirt
-ggmirt
+'report
+mirt
 ggcorrplot
 widyr
 ggbeeswarm
@@ -48,27 +61,29 @@ tinytex'
 precompiled_repos=\
 "repos = c('https://apache.r-universe.dev', 'https://cloud.r-project.org', 'https://ddsjoberg.r-universe.dev')"
 
+github_remotes="
+if(!require(ggmirt)) remotes::install_github('masurp/ggmirt', update = 'never')"
+
 install_with_apt()
 {
 	apt-get update
 	apt-get clean
 	apt-get -y --no-install-recommends install r-base
 
-
-
 ## REMOVE INITIAL AND FINAL NEWLINE CHARACTERS AND REPLACE NEWLINES WITH SPACES
-packages=$(echo "$debian_common" | sed '1s/^[ \t]*//; $s/[ \t]*$//; s/\n/ /g')
+packages=$(echo "$debian_common" | tr '\n' ' ' | sed '1s/^[ \t]*//; $s/[ \t]*$//; s/\n/ /g')
+echo "$packages"
 ## LOOP THROUGH THE LIST
 echo "Packages to be installed:"
+
+## NOTE THAT THE PAKAGES VARIABLE IS UNQUOTED, THIS IF NEED TO SEPARATE BY SPACES _ THE QUOTATION SHOWSS A SINGEL STRING
 for package in $packages
 do
-    echo ___TO_BE_I__$package
-	apt-get -y install "$pack" || echo __DEBIAN_INSTALLED__"$pack"
-    echo ___INSTALLED___$package
+    echo ___TO_BE_I__"$package"
+	apt-get install -y --no-install-recommends "$package" || echo __DEBIAN_INSTALLED__"$package"
+    echo ___INSTALLED___"$package"
 	apt-get clean
 done
-
-
 
 
 #apt-get install -y r-base-dev || echo __APTTED__"$pack"
@@ -80,8 +95,8 @@ apt-get clean || echo __APTTED__"$pack"
 #apt --fix-broken install -y || echo __APTTED__"$pack"
 echo "$debian_common" | while read -r pack
 do 
-	echo __DEBIAN_TO_I__"$pack"
-	#apt-get -y install "$pack" || echo __DEBIAN_INSTALLED__"$pack"
+	echo __DEBIAN_TO_I__"$package"
+	#apt-get -y install "$package" || echo __DEBIAN_INSTALLED__"$package"
 	#apt-get clean
 done
 
@@ -111,8 +126,9 @@ do
 	Rscript -e "if(!require('${pack}')) install.packages('${pack}', ${precompiled_repos})"
 done
 
-	Rscript -e "tinytex::install_tinytex()"
 	Rscript -e "if(!require(hugodown)) remotes::install_github('r-lib/hugodown', update = 'never')"
+	Rscript -e "tinytex::install_tinytex(force= TRUE)"
+	Rscript -e "if(!require(ggmirt)) remotes::install_github('masurp/ggmirt', update = 'never')"
 }
 
 ## CHECK DEBUG FLAG
@@ -128,8 +144,9 @@ else
 	require_conditional_install
 fi
 
+#Rscript -e "tinytex::install_tinytex(force= TRUE)"
+apt-get autoremove -y
 apt-get clean
-
 ## arm
 #https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_0.80.0_Linux-ARM.tar.gz
 ## arch64v8
